4f995ecad3b8b9b659ddbcc816557fdb3b17d524
evjeny <gkanafing@gmail.com>
2022-04-05 00:38:48 +0500

    add data extractor

---
+++ data_extractor.py
@@ -0,0 +1,75 @@
+import sqlite3
+
+from dostoevsky.tokenization import RegexTokenizer
+from dostoevsky.models import FastTextSocialNetworkModel
+
+
+def create_message_replies_table(connection: sqlite3.Connection):
+    connection.executescript(
+        """
+        DROP TABLE IF EXISTS message_replies;
+        CREATE TABLE message_replies(
+            message_id INTEGER,
+            replies_count INTEGER
+        );
+        """
+    )
+    connection.commit()
+
+
+def extract_message_replies(connection: sqlite3.Connection):
+    connection.execute(
+        """
+        WITH unique_ids AS (
+            SELECT DISTINCT message_id FROM answer
+        )
+
+        INSERT INTO message_replies(message_id, replies_count)
+        SELECT reply_to_msg_id, COUNT(*) FROM answer
+        WHERE reply_to_msg_id IN unique_ids AND text != ""
+        GROUP BY reply_to_msg_id
+        """
+    )
+
+
+def create_sentiment_table(connection: sqlite3.Connection):
+    connection.executescript(
+        """
+        DROP TABLE IF EXISTS sentiment;
+        CREATE TABLE sentiment(
+            message_id INTEGER,
+            value REAL
+        );
+        """
+    )
+    connection.commit()
+
+
+def extract_sentiments(connection: sqlite3.Connection):
+    tokenizer = RegexTokenizer()
+    model = FastTextSocialNetworkModel(tokenizer=tokenizer)
+
+    def _text_to_sentiment(text: str) -> float:
+        s = model.predict([text])[0]
+        return s.get("positive", 0) - s.get("negative", 0)
+    
+    connection.create_function("SENTIMENT", 1, _text_to_sentiment)
+    connection.execute(
+        """
+        INSERT INTO sentiment(message_id, value)
+        SELECT message_id, SENTIMENT(text) FROM answer
+        """
+    )
+
+
+def main(db_name: str):
+    with sqlite3.connect(db_name) as connection:
+        create_message_replies_table(connection)
+        extract_message_replies(connection)
+
+        create_sentiment_table(connection)
+        extract_sentiments(connection)
+
+
+if __name__ == "__main__":
+    main("result.db")
